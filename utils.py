f171854f88bfab6afdb3e6efd6147d265d93fe76
InfoTech <90517911+infotechIT@users.noreply.github.com>
2021-10-21 17:16:24 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -192,12 +192,12 @@ async def upload(m, bot, chat_id, dir):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-					InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/itzmerixo'),
+					InlineKeyboardButton("🤖Other Bots", url="https://t.me/infotechbots/122")
 				],
 				[
-					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/subin_works")
+					InlineKeyboardButton("🔗Source Code", url="https://github.com/infotechIT/Instagram-Bot"),
+                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/infotechIT")
 				]
 			]
 			)
